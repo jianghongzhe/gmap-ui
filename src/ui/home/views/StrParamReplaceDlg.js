fdbe384084ef40af6dda1e1afaf809d8158c1a30
小蒋 <jianghongzhegigi@163.com>
2021-09-04 16:38:32 +0800

    opt: url参数设置对话框：打开后第一个输入框默认获得焦点；每个输入框回车后焦点转到下一输入框，最后一个输入框回车后相当于确认

--- src/ui/home/views/StrParamReplaceDlg.js
+++ src/ui/home/views/StrParamReplaceDlg.js
@@ -1,5 +1,5 @@
 /** @jsxImportSource @emotion/react */
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { Input, Modal} from 'antd';
 import {withEnh} from '../../common/specialDlg';
 import strTmpl from '../../../common/strTmpl';
@@ -9,17 +9,37 @@ const EnhDlg=withEnh(Modal);
 /**
  * 参数替换设置的对话框
  * @param {*} props {
- *  replItems,
-    currLinkUrl
+ *  replItems:[
+ *      {
+ *          name: "1" 
+ *          def: ""
+ *          value: "" 
+ *          matchPart: "{{1}}"
+ *      } 
+ *  ],
+ *  currLinkUrl: "cmd://ping {{1}}.{{2}}.{{3}}.{{4}}"
  * }
  * @returns 
  */
 const StrParamReplaceDlg=(props)=>{
     const [items, setItems]=useState([]);
-
+    
+    /**
+     * props中给出的初始值转化为state值
+     */
     useEffect(()=>{
         setItems(props.replItems);
     },[props.replItems, setItems]);
+
+    /**
+     * 显示时给第一个输入框获得焦点
+     */
+    useEffect(()=>{
+        if(props.visible){
+            setFocus("paramdlg-0", 500);
+        }
+    },[props.visible]);
+    
 
     /**
      * 计算预览结果地址
@@ -27,6 +47,7 @@ const StrParamReplaceDlg=(props)=>{
     const previewUrl=useMemo(()=>{
         return strTmpl.replace(props.currLinkUrl, items);
     },[items, props.currLinkUrl]);
+
     
     /**
      * 占位符值更改事件
@@ -38,6 +59,8 @@ const StrParamReplaceDlg=(props)=>{
         newItems[ind].value=e.target.value;
         setItems(newItems);
     },[items, setItems]);
+
+
 
     return (
         <EnhDlg 
@@ -55,10 +78,14 @@ const StrParamReplaceDlg=(props)=>{
                 </div>
                 {
                     items.map((item, ind)=>(
-                        <div css={{marginTop:'15px'}}>
+                        <div key={ind} css={{marginTop:'15px'}}>
                             <div><span css={{fontWeight:'bold'}}>参数{ind+1} - {item.name}：</span></div>
                             <div css={{marginTop:'5px'}}>
-                                <Input css={{width:'96%'}} value={item.value} onChange={onChange.bind(this, ind)} placeholder="请输入参数值"/>
+                                <Input  id={`paramdlg-${ind}`} 
+                                        onPressEnter={ind<items.length-1 ? setFocus.bind(this, `paramdlg-${ind+1}`) : props.onOk.bind(this, previewUrl)} 
+                                        css={{width:'96%'}} 
+                                        value={item.value} 
+                                        onChange={onChange.bind(this, ind)} placeholder="请输入参数值"/>
                             </div>
                         </div>
                     ))
@@ -66,6 +93,27 @@ const StrParamReplaceDlg=(props)=>{
             </div>
         </EnhDlg>
     );
+};
+
+/**
+ * 使指定id的元素获得焦点。如果指定了延迟时间，则延迟一段时间后再获取；否则直接获取
+ */
+const setFocus=(id, delay=0)=>{
+    const doFocus=(id)=>{
+        const ele=document.querySelector(`#${id}`);
+        if(ele){
+            ele.focus();
+            return true;
+        }
+        return false;
+    };
+    if(delay){
+        setTimeout(() => {
+            doFocus(id);
+        }, delay);
+        return;
+    }
+    doFocus(id);
 };
 
 export default React.memo(StrParamReplaceDlg);
