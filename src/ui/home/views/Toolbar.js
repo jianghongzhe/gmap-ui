46f73e5e381f4923d2c507bf364e618a923d5637
小蒋 <jianghongzhegigi@163.com>
2022-04-06 20:38:00 +0800

    fix: #I515U5 导图浏览窗口中版本发布说明按钮点击无效

--- src/ui/home/views/Toolbar.js
+++ src/ui/home/views/Toolbar.js
@@ -5,6 +5,7 @@ import { PlusOutlined, FolderOpenOutlined, EditOutlined,LinkOutlined, FolderOutl
 import { tabCurrPaneAllNodesExpand, tabCurrPaneExpandStateChanged} from '../../../store/tabs';
 import {useRecoilValue} from 'recoil';
 import { useExpandAll, useRestoreDefaultExpandState } from '../../../hooks/tabs';
+import api from '../../../service/api';
 
 const { Header } = Layout;
 
@@ -12,47 +13,61 @@ const { Header } = Layout;
  * 工具栏
  * @param {*} props 
  */
-const Toolbar=(props)=>{
+const Toolbar=({
+        onShowNewMapDlg, 
+        onShowSelMapDlg, 
+        onShowDir, 
+        onShowCmd, 
+        onShowDevTool, 
+        onReloadApp, 
+        onShowEditMapDlg, 
+        onCopyMapLink, 
+        onScreenShot,
+        onExpImage,
+        onExpPdf,
+        onExpWord,
+        onExpMarkdown,
+        onExpHtml,
+        onCheckUpdate,
+        onOpenHelpDlg })=>{
+
     const allNodesExpand= useRecoilValue(tabCurrPaneAllNodesExpand);
     const expStateChanged=useRecoilValue(tabCurrPaneExpandStateChanged);
     const expandAll= useExpandAll();
     const restoreDefaultExpandState= useRestoreDefaultExpandState();
     
-
-
-    
     return (
         <Header css={headerStyle}>
-            <ToolbarItem title='新建' icon={<PlusOutlined />} className='toolbtnFirst' onClick={props.onShowNewMapDlg}/>
-            <ToolbarItem title='打开' icon={<FolderOpenOutlined />} onClick={props.onShowSelMapDlg}/>           
+            <ToolbarItem title='新建' icon={<PlusOutlined />} className='toolbtnFirst' onClick={onShowNewMapDlg}/>
+            <ToolbarItem title='打开' icon={<FolderOpenOutlined />} onClick={onShowSelMapDlg}/>           
 
             <Divider type="vertical" className='divider'/>
-            <ToolbarItem title='打开目录' icon={<FolderOutlined />} onClick={props.onShowDir}/>
-            <ToolbarItem title='打开控制台' icon={<CodeOutlined />} onClick={props.onShowCmd}/>
-            <ToolbarItem title='开发者工具' icon={<ControlOutlined />} onClick={props.onShowDevTool}/>
-            <ToolbarItem title='重新载入应用' icon={<ReloadOutlined />} onClick={props.onReloadApp}/>
+            <ToolbarItem title='打开目录' icon={<FolderOutlined />} onClick={onShowDir}/>
+            <ToolbarItem title='打开控制台' icon={<CodeOutlined />} onClick={onShowCmd}/>
+            <ToolbarItem title='开发者工具' icon={<ControlOutlined />} onClick={onShowDevTool}/>
+            <ToolbarItem title='重新载入应用' icon={<ReloadOutlined />} onClick={onReloadApp}/>
             
             
             <Divider type="vertical" className='divider'/>
-            <ToolbarItem title='编辑' icon={<EditOutlined />} onClick={props.onShowEditMapDlg}/>
-            <ToolbarItem title='复制导图链接' icon={<LinkOutlined />} onClick={props.onCopyMapLink}/>
+            <ToolbarItem title='编辑' icon={<EditOutlined />} onClick={onShowEditMapDlg}/>
+            <ToolbarItem title='复制导图链接' icon={<LinkOutlined />} onClick={onCopyMapLink}/>
             <ToolbarItem title='恢复节点默认状态' disabled={!expStateChanged} type="primary" icon={<CompressOutlined />} onClick={restoreDefaultExpandState}/>
             <ToolbarItem title='展开全部节点' disabled={allNodesExpand} type="primary" icon={<ExpandOutlined />} onClick={expandAll}/>
                       
 
 
             <Divider type="vertical" className='divider'/>
-            <ToolbarItem title='滚动截屏' icon={<CameraOutlined />} onClick={props.onScreenShot}/>
-            <ToolbarItem title='导出图片' icon={<FileImageOutlined />} onClick={props.onExpImage}/>
-            <ToolbarItem title='导出pdf' icon={<FilePdfOutlined />} onClick={props.onExpPdf}/>
-            <ToolbarItem title='导出word' icon={<FileWordOutlined />} onClick={props.onExpWord}/>
-            <ToolbarItem title='导出markdown' icon={<FileMarkdownOutlined />} onClick={props.onExpMarkdown}/>
-            <ToolbarItem title='导出html' icon={<Html5Outlined />} onClick={props.onExpHtml}/>
+            <ToolbarItem title='滚动截屏' icon={<CameraOutlined />} onClick={onScreenShot}/>
+            <ToolbarItem title='导出图片' icon={<FileImageOutlined />} onClick={onExpImage}/>
+            <ToolbarItem title='导出pdf' icon={<FilePdfOutlined />} onClick={onExpPdf}/>
+            <ToolbarItem title='导出word' icon={<FileWordOutlined />} onClick={onExpWord}/>
+            <ToolbarItem title='导出markdown' icon={<FileMarkdownOutlined />} onClick={onExpMarkdown}/>
+            <ToolbarItem title='导出html' icon={<Html5Outlined />} onClick={onExpHtml}/>
 
             <Divider type="vertical" className='divider'/>
-            <ToolbarItem title='版本发布说明' icon={<HistoryOutlined />} onClick={props.openReleaseNote}/>
-            <ToolbarItem title='检查更新' icon={<CloudSyncOutlined />} onClick={props.onCheckUpdate}/>
-            <ToolbarItem title='帮助' icon={<QuestionOutlined />} onClick={props.onOpenHelpDlg}/>
+            <ToolbarItem title='版本发布说明' icon={<HistoryOutlined />} onClick={api.openReleaseNote}/>
+            <ToolbarItem title='检查更新' icon={<CloudSyncOutlined />} onClick={onCheckUpdate}/>
+            <ToolbarItem title='帮助' icon={<QuestionOutlined />} onClick={onOpenHelpDlg}/>
         </Header>
     );
     
