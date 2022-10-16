0da91569881593fa41fa8762c2d01270d9b494ad
jianghongzhegigi@163.com <jianghongzhegigi@163.com>
2022-10-16 12:17:52 +0800

    fix: 修复了升级react18后导致的编辑器功能异常的问题，已降级到react17.0.2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const root =ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root =ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
+ReactDOM.render(
     <React.StrictMode>
         <App />
-    </React.StrictMode>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
 
 
 // If you want your app to work offline and load faster, you can change
